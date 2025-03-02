11850282cd80d888c5fa44e759709aacc2aefb16
iamtayyab007 <tayyabm336@gmail.com>
2025-03-02 23:50:04 +0500

    updated

--- Frontend/vite-project/src/api/external.js
+++ Frontend/vite-project/src/api/external.js
@@ -4,7 +4,7 @@ const NEWS_API_ENDPOINT = `https://newsapi.org/v2/everything?q=business AND bloc
 
 const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 const CRYPTO_API_ENDPOINT =
-  "https://pro-api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
 export const getNews = async () => {
   try {
     const response = await axios.get(NEWS_API_ENDPOINT);
